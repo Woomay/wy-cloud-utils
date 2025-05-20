eaddb5f572aff80239881b91f1683164d18f5924
Yanxiang Hu <yanxiang.hu@xtalpi.com>
2025-05-20 09:04:55 +0800

    feat: 添加随机颜色和随机方向函数

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export function random(min: number, max:number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+export function randomDirection() {
+  return Math.random() < 0.5 ? 1 : -1;
+}
